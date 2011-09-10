74827531a464ddc9906cd97f66d2919845e75753
Burke Libbey <burke@burkelibbey.org>
2011-09-10 12:00:49 -0500

    session 2

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,55 @@
-class App
+class Universe # TODO: Rename
+  def initialize(*cells)
+    @universe = {}
+    cells.each do |cell|
+      @universe[cell.x] ||= {}
+      @universe[cell.x][cell.y] = cell
+    end
+  end
+  
+  def get_cell_at(x, y)
+    cell = (@universe[x] || {})[y]
+    cell || Cell.new(x, y, :dead)
+  end
+  
+  def neighbours_to(cell)
+    neighbors = []
+    (-1..1).each do |x_offset|
+      (-1..1).each do |y_offset|
+        next if x_offset == 0 && y_offset == 0
+        neighbors << get_cell_at(x_offset + cell.x, y_offset + cell.y)
+      end
+    end
+    neighbors
+  end
+end
 
-
+class Cell
+  attr_reader :x, :y, :state
+  def initialize (x, y, state = :alive)
+    @x = x
+    @y = y
+    @state = state
+  end
+  
+  def alive?
+    state == :alive
+  end
+  
+  def dead?
+    !alive?
+  end
+  
+  def next_generation(universe)
+    neighbours = universe.neighbours_to(self)
+    next_state = case neighbours.select(&:alive?).size
+    when 0..1
+      :dead
+    when 2..3
+      :alive
+    when 4..8
+      :dead
+    end
+    Cell.new(x,y, next_state)
+  end
 end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,7 +1,40 @@
 require './lib/app'
+ 
 
-describe "Something" do
-  it "should be defined" do
-    App.should_not be_nil
+describe "The Game of Life" do
+  
+  subject { 
+    Universe.new(Cell.new(0,0), Cell.new(1,0), Cell.new(2,0), Cell.new(1,1), Cell.new(0,2))
+  }
+  
+  it "creates a new Universe with a collection of currently-living cells" do
+    Universe.new(Cell.new(0,0), Cell.new(1,0), Cell.new(2,0), Cell.new(1,1), Cell.new(0,2))
   end
-end
+  
+  it "knows the state of its living cells" do
+    subject.get_cell_at(0,0).should be_alive
+    # cell.alive? 
+  end
+
+  it "knows when a cell is not alive" do
+    subject.get_cell_at(5,5).should be_dead
+    # cell.dead?
+  end
+  
+  it "kills a living cell with less than 2 living neighbors" do
+    subject.get_cell_at(0,2).next_generation(subject).should be_dead
+  end 
+  
+  it "any live cell with 2 or 3 neighbors lives on to the next generation" do
+    subject.get_cell_at(2,0).next_generation(subject).should be_alive
+  end
+  
+  it "returns the neighbours to a given cell" do
+    cell = subject.get_cell_at(2,0)
+    subject.neighbours_to(cell).first.should be_instance_of(Cell)
+  end
+  
+  it "any live cell with more than 3 live neighbors dies" do
+    subject.get_cell_at(1,1).next_generation(subject).should be_dead
+  end
+end 
